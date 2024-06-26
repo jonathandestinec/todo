c84341cae5379707b3a9f5e327b61d1cf09aedb7
jonathandestinec <jonathandestinec@gmail.com>
2024-06-26 22:54:17 +0100

    fixed versel error

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,10 +2,10 @@
 
 import { createClient } from "@/utils/supabase/server"
 import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { redirect } from "next/navigation"
 
-export async function GET(res: NextResponse) {
+export async function GET(req: NextRequest) {
 
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
